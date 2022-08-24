42b2994132ae255e435ac2334ef9fe42c9b590fb
madpractical1_21012012013 <maheshpillai404@gmail.com>
2022-08-24 23:09:56 +0530

    with output

---
+++ src/main/kotlin/Q3.kt
@@ -0,0 +1,34 @@
+fun main(){
+    println("Enter your Enrollment Number:")
+    var enrnumber = readLine()
+    println("Enter your Name:")
+    var name = readLine()
+    println("Enter your Branch:")
+    var branch = readLine()
+    println("Enter your Class:")
+    var classs = readLine()
+    println("Enter your Batch:")
+    var batch = readLine()
+    println("Enter your College Name:")
+    var college = readLine()
+    println("Enter your University Name:")
+    var university = readLine()
+    println("Enter your Age:")
+    var age= readLine()
+
+    println("===============")
+
+    println("Student's Data")
+
+    println("Your Enrollment Number : $enrnumber")
+    println("Your Name : $name")
+    println("Your Branch : $branch")
+    println("Your Class : $classs")
+    println("Your Batch : $batch")
+    println("Your College : $college")
+    println("Your University : $university")
+    println("Your Age : $age")
+
+
+
+}
